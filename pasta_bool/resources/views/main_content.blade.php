46e542e6e6e9d6738f93397caa013b7b5ea9d2b7
andreacapntd <andreacapntd@gmail.com>
2020-09-18 16:48:21 +0200

    ok stampa foto da database

--- pasta_bool/resources/views/main_content.blade.php
+++ pasta_bool/resources/views/main_content.blade.php
@@ -2,4 +2,56 @@
 
 @section('content')
 
-@endsection  
+  <div class="pastas">
+
+    <h1>Le lunghe</h1>
+
+    <div class="product">
+
+      @foreach ($data as $pasta)
+
+        @if ($pasta['tipo'] == "lunga")
+          <img src="{{ asset($pasta['src']) }}" alt="">
+        @endif
+
+      @endforeach
+
+    </div>
+  </div>
+
+  <div class="pastas">
+
+    <h1>Le corte</h1>
+
+    <div class="product">
+
+      @foreach ($data as $pasta)
+
+        @if ($pasta['tipo'] == "corta")
+          <img src="{{ asset($pasta['src']) }}" alt="">
+        @endif
+
+      @endforeach
+
+    </div>
+  </div>
+
+  <div class="pastas">
+
+    <h1>Le cortissime</h1>
+
+    <div class="product">
+
+      @foreach ($data as $pasta)
+
+        @if ($pasta['tipo'] == "cortissima")
+          <img src="{{ asset($pasta['src']) }}" alt="">
+        @endif
+
+      @endforeach
+
+    </div>
+  </div>
+
+
+@endsection
