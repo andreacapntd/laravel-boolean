ad700733836e27f4958fde18d9fdb7c418ef83da
andreacapntd <andreacapntd@gmail.com>
2020-09-18 15:01:52 +0200

    scaffolding + header

---
+++ pasta_bool/resources/views/layouts/main_layout.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Pasta Bool</title>
+
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+  </head>
+  <body>
+
+    @include('partials.header')
+
+    <main>
+      @yield('content')
+    </main>
+
+    @include('partials.footer')
+
+  </body>
+</html>
---
+++ pasta_bool/resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+  <div class="logo">
+
+    <img src="{{ asset('images/marchio-sito-test.png') }}" alt="">
+    
+  </div>
+  <nav id="navbar">
+
+    <ul id="navlist">
+
+      <li>Home</li>
+      <li>Prodotti</li>
+      <li>News</li>
+
+    </ul>
+  </nav>
+</header>
